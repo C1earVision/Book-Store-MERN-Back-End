3fd33402f49f15acb508c785416d7de1b804110c
C1earVision <ahmedebrahim3690@gmail.com>
2023-10-29 11:04:49 +0200

    update

--- controllers/no-auth.js
+++ controllers/no-auth.js
@@ -41,7 +41,7 @@ const getAllBooks = async (req, res)=>{
     });
   }
   const page = Number(req.query.page) || 1
-  const limit = 10
+  const limit = 12
   const skip = (page-1) * limit;
   result = result.skip(skip).limit(limit)
 
