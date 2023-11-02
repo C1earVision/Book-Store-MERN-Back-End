f1265695922256a85afd626ef7c1e5ce58e68865
C1earVision <ahmedebrahim3690@gmail.com>
2023-11-02 12:17:02 +0200

    update

--- controllers/req-auth.js
+++ controllers/req-auth.js
@@ -9,9 +9,8 @@ const addBook = async (req,res)=>{
   if(!admin){
     throw new CustomAPIError('this user has no access to this route', StatusCodes.UNAUTHORIZED)
   }
-  req.body.genre = req.body.genre.split(', ').sort().join(', ')
   req.body.createdBy = userId
-  const book = await Books.create(req.body)
+  const book = await Books.create({...req.body, genre:req.body.genre.split(', ').sort().join(', ')})
   res.status(StatusCodes.CREATED).json({book})
 }
 
